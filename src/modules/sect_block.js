edebb057123f0ca0d191419f2e3670e14ae7b24b
leo hsieh <bardiche4768@gmail.com>
2023-01-29 20:57:04 +0800

    done viewall anchor

--- src/modules/sect_block.js
+++ src/modules/sect_block.js
@@ -1,7 +1,7 @@
 import "../css/sect_block.css";
 import ColourRGBA from "../config/colour_rgba";
 import { wrapDivRecursive, wrapDiv, wrapLanguages} from "../utils";
-import { fadeInLeftwards, fadeInRightwards } from "./defaults/entrance_effect";
+import { fadeIn, fadeInLeftwards, fadeInRightwards } from "./defaults/entrance_effect";
 import Button from "./button";
 
 export default class SectBlock{
@@ -22,18 +22,16 @@ export default class SectBlock{
 
     get(item){
         var header = [];
-
         var langs = wrapLanguages(this.title);
         langs = wrapDiv("title", langs);
-        langs = fadeInLeftwards.get(langs);
         header.push(langs);
         if (this.viewAll) {
             var all = new Button({jp: "View All", en: "View All"});
             header.push(wrapDiv("view-all", all.get(this.url)));
         }
-            
 
         header = wrapDiv("header", header);
+        header = fadeIn.get(header);
         item = wrapDiv("content", item);
         return wrapDiv("sect-block", header, item);
     }
